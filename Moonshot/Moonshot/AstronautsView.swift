8833ea0c1693cc4efa2b6cde3641ace3038932c4
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-10 08:39:20 +0200

    first attempt to show mission on the view

--- Moonshot/Moonshot/AstronautsView.swift
+++ Moonshot/Moonshot/AstronautsView.swift
@@ -10,10 +10,37 @@ import SwiftUI
 struct AstronautsView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack {
-            Text("^[Hello, \(astronauts.count) astronaut!](inflect: true)")
-            Text("^[completed, \(missions.count) mission!](inflect: true)")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("MoonShot")
         }
     }
 }
